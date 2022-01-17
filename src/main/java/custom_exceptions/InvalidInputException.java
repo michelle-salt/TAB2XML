82adee4a8a2947cc43e35810e4cb644eff423737
hpaurobally <hpaurobally@gmail.com>
2022-01-17 13:01:06 -0500

    Added another comment to the IIE class

--- src/main/java/custom_exceptions/InvalidInputException.java
+++ src/main/java/custom_exceptions/InvalidInputException.java
@@ -3,7 +3,9 @@ package custom_exceptions;
 @SuppressWarnings("serial")
 public class InvalidInputException extends Exception{
 
-	/* This exception is called InvalidInputException */
+	/* This exception is called InvalidInputException 
+	 * and inherits from the Exception class.
+	 */
     public InvalidInputException(String msg){
         super(msg);
     }
