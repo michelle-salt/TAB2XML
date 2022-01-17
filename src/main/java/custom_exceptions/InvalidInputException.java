9b4f7ec270c34ecc89a6b716592049c1720b3cc8
hpaurobally <hpaurobally@gmail.com>
2022-01-17 12:35:00 -0500

    Add comment to invalid input exception
    to practice pushing/pulling

--- src/main/java/custom_exceptions/InvalidInputException.java
+++ src/main/java/custom_exceptions/InvalidInputException.java
@@ -3,6 +3,7 @@ package custom_exceptions;
 @SuppressWarnings("serial")
 public class InvalidInputException extends Exception{
 
+	/* This exception is called InvalidInputException */
     public InvalidInputException(String msg){
         super(msg);
     }
