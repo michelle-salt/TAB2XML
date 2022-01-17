7447af46f03a30e6065a40173924370cc211b685
michelle-salt <cookies10m@gmail.com>
2022-01-17 11:37:34 -0500

    Add comment to preview button method
    Test commit created for lab 2, so others' can practice pulling

--- src/main/java/GUI/MainViewController.java
+++ src/main/java/GUI/MainViewController.java
@@ -308,6 +308,7 @@ public class MainViewController extends Application {
 	private void previewButtonHandle() throws IOException {
 		System.out.println("Preview Button Clicked!");
 		// converter.getMusicXML() returns the MusicXML output as a String
+		//This is a change to be pushed (for lab 2)
 	}
 
 	public void refresh() {
