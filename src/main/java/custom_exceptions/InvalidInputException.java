01d59e1b51ccc74b47fae70d9a4385ff4353d65a
hpaurobally <hpaurobally@gmail.com>
2022-01-19 15:22:35 -0500

    default

--- src/main/java/custom_exceptions/InvalidInputException.java
+++ src/main/java/custom_exceptions/InvalidInputException.java
@@ -3,10 +3,6 @@ package custom_exceptions;
 @SuppressWarnings("serial")
 public class InvalidInputException extends Exception{
 
-	/* This exception is called InvalidInputException 
-	 * and inherits from the Exception class. The method takes
-	 * a String parameter.
-	 */
     public InvalidInputException(String msg){
         super(msg);
     }
