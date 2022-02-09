c5e44847f86b476f47d12d71751d7d95cd8b2089
michelle-salt <cookies10m@gmail.com>
2022-02-09 05:09:54 -0500

    Create Clef class to store attributes for each clef
    Only includes elements found within the sample guitar tab and its corresponding MusicXML file

---
+++ src/main/java/musicxml/parsing/Clef.java
@@ -0,0 +1,21 @@
+package musicxml.parsing;
+
+public class Clef {
+
+	private String sign;
+	private int numLines;
+		
+	public Clef(String sign, int numLines) {
+		this.sign = sign;
+		this.numLines = numLines;
+	}
+	
+	//Getters
+	public String getSign() {
+		return sign;
+	}
+
+	public int getNumLines() {
+		return numLines;
+	}
+}
