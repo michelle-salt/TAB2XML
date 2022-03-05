844f2156779dc1f8befd215e93cce648b31e23ba
Duaa Ali <duaaa29@my.yorku.ca>
2022-03-05 15:10:44 -0500

    Finished the TestTemplate to be followed when creating test cases

--- src/test/java/parser/TestTemplate.java
+++ src/test/java/parser/TestTemplate.java
@@ -1,68 +1,46 @@
 package parser;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.junit.Test;
-
 import musicxml.parsing.*;
 
-public class TestTemplate {
-	protected Parser bendTest1, capricho, ex38, graceTest1, graceTest2, money, parabola, push, restTest1, tieTest1, timingTest1, wikiDrumTab, wikiGuitarTab;
-	
-//	public TestTemplate() throws IOException {
-//		String filePath = new File("").getAbsolutePath().concat("\\src\\test\\resources\\musicXMLFiles\\");
-//		bendTest1 = new Parser(Files.readString(Paths.get(filePath.concat("bendTest1.txt"))));
-//		capricho = new Parser(Files.readString(Paths.get(filePath.concat("capricho.txt"))));
-//		ex38 = new Parser(Files.readString(Paths.get(filePath.concat("ex38.txt"))));
-//		graceTest1 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest1.txt"))));
-//		graceTest2 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest2.txt"))));
-//		money = new Parser(Files.readString(Paths.get(filePath.concat("money.txt"))));
-//		parabola = new Parser(Files.readString(Paths.get(filePath.concat("parabola.txt"))));
-//		push = new Parser(Files.readString(Paths.get(filePath.concat("push.txt"))));
-//		restTest1 = new Parser(Files.readString(Paths.get(filePath.concat("restTest1.txt"))));
-//		tieTest1 = new Parser(Files.readString(Paths.get(filePath.concat("tieTest1.txt"))));
-//		timingTest1 = new Parser(Files.readString(Paths.get(filePath.concat("timingTest1.txt"))));
-//		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
-//		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
-//	}
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
-	@Test
-	public void test_wikiGuitarTab() throws IOException {
-		String filePath = new File("").getAbsolutePath().concat("\\src\\test\\resources\\musicXMLFiles\\");
-//		bendTest1 = new Parser(Files.readString(Paths.get(filePath.concat("bendTest1.txt"))));
-//		capricho = new Parser(Files.readString(Paths.get(filePath.concat("capricho.txt"))));
-//		ex38 = new Parser(Files.readString(Paths.get(filePath.concat("ex38.txt"))));
-//		graceTest1 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest1.txt"))));
-//		graceTest2 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest2.txt"))));
-//		money = new Parser(Files.readString(Paths.get(filePath.concat("money.txt"))));
-//		parabola = new Parser(Files.readString(Paths.get(filePath.concat("parabola.txt"))));
-//		push = new Parser(Files.readString(Paths.get(filePath.concat("push.txt"))));
-//		restTest1 = new Parser(Files.readString(Paths.get(filePath.concat("restTest1.txt"))));
-//		tieTest1 = new Parser(Files.readString(Paths.get(filePath.concat("tieTest1.txt"))));
-//		timingTest1 = new Parser(Files.readString(Paths.get(filePath.concat("timingTest1.txt"))));
-//		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
-//		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
-		
-//		Parser p = new Parser(this.wikiGuitarTab);
-//		assertEquals(2, this.wikiGuitarTab.getNumMeasures());
-		
-		assertEquals(2, 2);
-	}
-	
+public class TestTemplate {	
 	/*
 	 * When creating a new test class,
 	 * 		1. Copy all imports
-	 * 		2. Add "extends TestTemplate" in the class declaration
-	 * 		3. Copy the block below into your new class, and uncomment it
-	 * 		4. Create a new test for every one of the variables above
+	 * 		2. Copy the block below into your new class, and uncomment it
+	 * 		3. Create a new test for both the wikiDrumTab and wikiGuitarTab
+	 * 		4. Create test cases using the template block shown below the setUp method
+	 * 			Remember to change the method name!	
 	 */
-//	@Before
-//	public void setUp() throws Exception {
-//		super();
+//	@BeforeEach
+//	void setUp() throws Exception {
+//		//Get the OS
+//        String OS = System.getProperty("os.name").toLowerCase();
+//        //Get the filepath of all MusicXML files, based on the device's OS
+//        String filePath = new File("").getAbsolutePath();
+//        //Insert "\\" in path
+//        if (OS.contains("win")) {
+//            filePath = filePath.concat("\\src\\test\\resources\\musicXMLFiles\\");
+//        }
+//        //Assumed to be unix (mac or linux)
+//        //Insert "/" in path
+//        else {
+//            filePath = filePath.concat("/src/test/resources/musicXMLFiles/");
+//        }
+//		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
+//		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
+//	}
+//	
+//	@Test
+//	public void testInsertNameHere() {
+//		assertEquals(expectedValue, this.wikiGuitarTab.methodNameAsNeeded());
+//		assertEquals(expectedValue, this.wikiDrumTab.methodNameAsNeeded())
 //	}
 }
