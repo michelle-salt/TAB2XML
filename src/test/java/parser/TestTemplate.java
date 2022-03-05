24ac13bdcf240fef53be3649426c657ba7775153
Vincent Mai <maiv8964@my.yorku.ca>
2022-03-05 16:22:53 -0500

    pushed to pull develop

--- src/test/java/parser/TestTemplate.java
+++ src/test/java/parser/TestTemplate.java
@@ -14,22 +14,22 @@ import musicxml.parsing.*;
 public class TestTemplate {
 	protected Parser bendTest1, capricho, ex38, graceTest1, graceTest2, money, parabola, push, restTest1, tieTest1, timingTest1, wikiDrumTab, wikiGuitarTab;
 	
-//	public TestTemplate() throws IOException {
-//		String filePath = new File("").getAbsolutePath().concat("\\src\\test\\resources\\musicXMLFiles\\");
-//		bendTest1 = new Parser(Files.readString(Paths.get(filePath.concat("bendTest1.txt"))));
-//		capricho = new Parser(Files.readString(Paths.get(filePath.concat("capricho.txt"))));
-//		ex38 = new Parser(Files.readString(Paths.get(filePath.concat("ex38.txt"))));
-//		graceTest1 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest1.txt"))));
-//		graceTest2 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest2.txt"))));
-//		money = new Parser(Files.readString(Paths.get(filePath.concat("money.txt"))));
-//		parabola = new Parser(Files.readString(Paths.get(filePath.concat("parabola.txt"))));
-//		push = new Parser(Files.readString(Paths.get(filePath.concat("push.txt"))));
-//		restTest1 = new Parser(Files.readString(Paths.get(filePath.concat("restTest1.txt"))));
-//		tieTest1 = new Parser(Files.readString(Paths.get(filePath.concat("tieTest1.txt"))));
-//		timingTest1 = new Parser(Files.readString(Paths.get(filePath.concat("timingTest1.txt"))));
-//		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
-//		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
-//	}
+	public TestTemplate() throws IOException {
+		String filePath = new File("").getAbsolutePath().concat("\\src\\test\\resources\\musicXMLFiles\\");
+		bendTest1 = new Parser(Files.readString(Paths.get(filePath.concat("bendTest1.txt"))));
+		capricho = new Parser(Files.readString(Paths.get(filePath.concat("capricho.txt"))));
+		ex38 = new Parser(Files.readString(Paths.get(filePath.concat("ex38.txt"))));
+		graceTest1 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest1.txt"))));
+		graceTest2 = new Parser(Files.readString(Paths.get(filePath.concat("graceTest2.txt"))));
+		money = new Parser(Files.readString(Paths.get(filePath.concat("money.txt"))));
+		parabola = new Parser(Files.readString(Paths.get(filePath.concat("parabola.txt"))));
+		push = new Parser(Files.readString(Paths.get(filePath.concat("push.txt"))));
+		restTest1 = new Parser(Files.readString(Paths.get(filePath.concat("restTest1.txt"))));
+		tieTest1 = new Parser(Files.readString(Paths.get(filePath.concat("tieTest1.txt"))));
+		timingTest1 = new Parser(Files.readString(Paths.get(filePath.concat("timingTest1.txt"))));
+		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
+		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
+	}
 
 	@Test
 	public void test_wikiGuitarTab() throws IOException {
