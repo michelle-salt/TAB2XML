ffa12356ef3e74975e8e77fbb0f7247be088b10d
michelle-salt <cookies10m@gmail.com>
2022-03-24 16:04:05 -0400

    Fix TestTemplate
    Adds two necessary imports

--- src/test/java/parser/TestTemplate.java
+++ src/test/java/parser/TestTemplate.java
@@ -1,9 +1,13 @@
 package parser;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import org.junit.jupiter.api.*;
 
 import musicxml.parsing.*;
 
