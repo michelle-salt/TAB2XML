8df707c951ab9484a9e83eafb7ca88c5027eb4ca
Duaaa29 <76664117+Duaaa29@users.noreply.github.com>
2022-03-31 15:43:30 -0400

    Added the required elements onto the Customize GUI
    This includes labels, text boxes, radio buttons, and an "Apply" button!
    
    Co-Authored-By: michelle-salt <69985466+michelle-salt@users.noreply.github.com>

--- src/main/java/GUI/CustomizeController.java
+++ src/main/java/GUI/CustomizeController.java
@@ -1,12 +1,33 @@
 package GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
 public class CustomizeController {
     private PreviewSheetController mvc;
+    
+    @FXML
+    private RadioButton leftRadio;
+    @FXML
+    private RadioButton justRadio;
 
 	public CustomizeController() {
 	}
+	
 
     public void setPreviewSheetController(PreviewSheetController mvcInput) {
     	mvc = mvcInput;
+    }
+    
+    @FXML
+    private void initialize() {
+    	ToggleGroup group = new ToggleGroup();
+    	leftRadio.setToggleGroup(group);
+    	justRadio.setToggleGroup(group);
+    }
+    
+    public void applyButton() {
+    	
     }
  } 
