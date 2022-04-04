ae174cf70a4e6c2c655cc0808bb55e14574a5a42
hpaurobally <hpaurobally@gmail.com>
2022-04-04 01:28:09 -0400

    add support for single changes to either spacing

--- src/main/java/GUI/CustomizeController.java
+++ src/main/java/GUI/CustomizeController.java
@@ -2,38 +2,55 @@ package GUI;
 
 import java.io.IOException;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 public class CustomizeController {
-    private PreviewSheetController mvc;
-    
-    @FXML private RadioButton leftRadio;
-    @FXML private RadioButton justRadio;
-    
-    @FXML TextField noteSpacingField;
-    @FXML TextField staffSpacingField;
+	private PreviewSheetController mvc;
+
+	@FXML private RadioButton leftRadio;
+	@FXML private RadioButton justRadio;
+
+	@FXML TextField noteSpacingField;
+	@FXML TextField staffSpacingField;
+
+	BooleanProperty applyButtonPressed = new SimpleBooleanProperty(false);
 
 	public CustomizeController() {}
-	
-    public void setPreviewSheetController(PreviewSheetController mvcInput) {
-    	mvc = mvcInput;
-    }
-    
-    @FXML
-    private void initialize() {
-    	ToggleGroup group = new ToggleGroup();
-    	leftRadio.setToggleGroup(group);
-    	justRadio.setToggleGroup(group);
-    }
-    
-    @FXML
-    public void applyButton() throws IOException {
-    	mvc.setNoteSpacing(Integer.parseInt(noteSpacingField.getText()));
-    	mvc.setStaffSpacing(Integer.parseInt(staffSpacingField.getText()));
-    	mvc.update();
-    	mvc.convertWindow.hide();
-    }
- } 
+
+	public void setPreviewSheetController(PreviewSheetController mvcInput) {
+		mvc = mvcInput;
+	}
+
+	@FXML
+	private void initialize() {
+		ToggleGroup group = new ToggleGroup();
+		leftRadio.setToggleGroup(group);
+		justRadio.setToggleGroup(group);
+	}
+
+	@FXML
+	public <applyButtonPressed> void applyButton() throws IOException {
+		if (noteSpacingField.getText().isEmpty() && !staffSpacingField.getText().isEmpty()) {
+			mvc.setStaffSpacing(Integer.parseInt(staffSpacingField.getText()));
+			mvc.update();
+		}
+		else if (staffSpacingField.getText().isEmpty() && !noteSpacingField.getText().isEmpty()) {
+			mvc.setNoteSpacing(Integer.parseInt(noteSpacingField.getText()));
+			mvc.update();
+		}
+		else if (!staffSpacingField.getText().isEmpty() && !noteSpacingField.getText().isEmpty()) {
+			mvc.setNoteSpacing(Integer.parseInt(noteSpacingField.getText()));
+			mvc.setStaffSpacing(Integer.parseInt(staffSpacingField.getText()));
+			mvc.update();
+		}
+		else { 
+			// nothing
+		}
+		mvc.convertWindow.hide();
+	}
+} 
