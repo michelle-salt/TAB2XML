221ff01e920d62439f9ec8e38491c94ae649ec01
Joshua Hanif <90522357+jhaniff@users.noreply.github.com>
2022-04-11 22:26:07 -0400

    Added parenthesis to drum notes

--- src/main/java/GUI/DrawNotes.java
+++ src/main/java/GUI/DrawNotes.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.QuadCurve;
@@ -271,6 +273,14 @@ public class DrawNotes {
 			pane.getChildren().add(ellipse);
 		}
 		else {
+			if(this.note.getNoteheadParentheses()) {
+				Text text = new Text(x-8, y-1, "(");
+				Text text2 = new Text(x+12, y-1, ")");
+				text.setFont(Font.font("veranda", FontWeight.BLACK, 14));
+				text2.setFont(Font.font("veranda", FontWeight.BLACK, 14));
+				pane.getChildren().add(text);
+				pane.getChildren().add(text2);
+			}
 			Ellipse ellipse = new Ellipse(x+4, y-5, 6, 4.25);
 			ellipse.setRotate(320);
 			pane.getChildren().add(ellipse);
